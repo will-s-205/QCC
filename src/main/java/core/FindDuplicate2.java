375808f23f8750b148d0488b4a962d217fd174a3
William S <will.step.205@gmail.com>
2023-09-03 11:52:09 -0700

    FindDuplicate code refactored

--- src/main/java/core/FindDuplicate2.java
+++ src/main/java/core/FindDuplicate2.java
@@ -3,7 +3,7 @@ package core;
 import java.util.HashSet;
 import java.util.Set;
 
-public class FindDuplicates2 {
+public class FindDuplicate2 {
 
 	public boolean containsDuplicate(int[] nums) {
 		Set<Integer> set = new HashSet<Integer>();
@@ -20,6 +20,6 @@ public class FindDuplicates2 {
 	public static void main(String[] args) {
 		int arr[] = { 1, 2, 3, 4, 2, 7, 8, 8, 3 };
 
-		System.out.print(new FindDuplicates2().containsDuplicate(arr));
+		System.out.print(new FindDuplicate2().containsDuplicate(arr));
 	}
 }
--- src/main/java/core/FindDuplicates.java
+++ src/main/java/core/FindDuplicates.java
@@ -1,15 +1,47 @@
 package core;
 
-public class FindDuplicates {
+import java.util.*;
 
-	public static void main(String[] args) {
-		int arr[] = { 1, 2, 3, 4, 2, 7, 8, 8, 3 };
+class findDuplicates {
 
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] == arr[j])
-					System.out.print(arr[j] + " ");
-			}
-		}
-	}
+  public static int[] findDuplicates(int[] arr1, int[] arr2) {
+    Set<Integer> hset1 = new HashSet<>();
+    Set<Integer> hset2 = new HashSet<>();
+
+    // ADDING THE ELEMENTS OF THE FIRST ARRAY TO THE FIRST SET
+    for (int num : arr1) {
+      hset1.add(num);
+    }
+
+    // COMPARING THE TWO SETS AND ADDING THE COMMON ELEMENTS TO THE RESULT SET
+    for (int num : arr2) {
+      if (hset1.contains(num)) {
+        hset2.add(num);
+      }
+    }
+
+    // PRINT THE RESULT SET TO THE CONSOLE OUTPUT WINDOW (OPTIONAL)
+    for (Integer i : hset2) {
+      System.out.print(i + " ");
+    }
+
+    // CONVERTING THE RESULT SET TO AN ARRAY AND SORTING IT 
+    int[] arr3 = new int[hset2.size()];
+    int k = 0;
+    for (Integer i : hset2) {
+      arr3[k] = (int) i;
+      k++;
+    }
+    Arrays.sort(arr3);
+
+    // RETURN THE RESULT ARRAY
+    return arr3;
+  }
+
+  public static void main(String[] args) {
+    int[] arr1 = { 1, 2, 3, 5, 6, 7 };
+    int[] arr2 = { 3, 6, 7, 8, 20 };
+
+    findDuplicates(arr1, arr2);
+  }
 }
